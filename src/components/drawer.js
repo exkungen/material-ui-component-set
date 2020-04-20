ecefe4ca53b987706488b3db2c75b6e89f86c870
Chris Obdam <>
2020-04-20 12:08:25 +0200

    WIP 1001

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -13,6 +13,7 @@
     return (
       <div className={[classes.root, isDev && classes.dev].join(' ')}>
         {children}
+        <div className={classes.content}>content</div>
       </div>
     );
   })(),
@@ -24,9 +25,25 @@
         display: 'flex',
         '&> .MuiAppBar-root': {
           backgroundColor: 'black',
-          // marginLeft: drawerWidth,
+          marginLeft: drawerWidth,
           width: `calc(100% - ${drawerWidth}px)`,
         },
+        '&> div:first-child': {
+          position: 'fixed',
+          width: `calc(100% - ${drawerWidth}px)`,
+          marginLeft: drawerWidth,
+        },
+        '&> .MuiDrawer-root > .MuiDrawer-paper': {
+          width: drawerWidth,
+        },
+        '&> div:nth-of-type(2) > .MuiDrawer-root > .MuiDrawer-paper': {
+          width: drawerWidth,
+        },
+      },
+      content: {
+        marginLeft: drawerWidth,
+        paddingTop: '90px',
+        width: `calc(100% - ${drawerWidth}px)`,
       },
       dev: {
         height: '100vh',
